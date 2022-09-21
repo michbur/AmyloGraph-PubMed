87d56c300ae44dd948a19f6585485033e5c55e47
michbur <michalburdukiewicz@gmail.com>
2022-09-21 21:42:04 +0200

    added some comments

--- analysis.R
+++ analysis.R
@@ -1,5 +1,7 @@
 library(dplyr)
 
+# seed: hour of submission
+
 set.seed(22092022)
 
 dat <- rbind(read.csv("inhibition.csv"),
@@ -8,8 +10,13 @@ dat <- rbind(read.csv("inhibition.csv"),
   select(DOI, Title) %>% 
   mutate(DOI = tolower(DOI))
 
+# number of all articles
+dim(dat)
+
+# articles present in the AmyloGraph database
 sum(dat[["DOI"]] %in% unique(tolower(AmyloGraph::ag_data_interactions[["doi"]])))
 
+# 200 randomly sampled publications
 part_dat <- filter(dat, !(DOI %in% unique(tolower(AmyloGraph::ag_data_interactions[["doi"]])))) %>% 
   slice(sample(1L:nrow(.), 200)) %>% 
   mutate(relevant = "") 
